1ef271cfe4151c58d7f412db17099e95e4f02a61
Emma Baskerville <ecbaskerville@yahoo.com>
2020-02-18 23:35:00 +0000

    Implement class Countries, instantiate object c in class Program

--- Program.cs
+++ Program.cs
@@ -9,36 +9,6 @@ namespace votingCalculator
         {
             Console.WriteLine("Welcome to the voting calculator");
             Console.WriteLine("Please set the votes for each country (y/n/a)");
-            Console.WriteLine("");
-
-            Dictionary<string, double> countryDict = new Dictionary<string, double>();
-            countryDict.Add("Austria", 1.98);
-            countryDict.Add("Belgium", 2.56);
-            countryDict.Add("Bulgaria", 1.56);
-            countryDict.Add("Croatia", 0.91);
-            countryDict.Add("Cyprus", 0.20);
-            countryDict.Add("Czech Republic", 2.35);
-            countryDict.Add("Denmark", 1.30);
-            countryDict.Add("Estonia", 0.30);
-            countryDict.Add("Finland", 1.23);
-            countryDict.Add("France", 14.98);
-            countryDict.Add("Germany", 18.54);
-            countryDict.Add("Greece", 2.40);
-            countryDict.Add("Hungary", 2.18);
-            countryDict.Add("Ireland", 1.10);
-            countryDict.Add("Italy", 13.65);
-            countryDict.Add("Latvia", 0.43);
-            countryDict.Add("Lithuania", 0.62);
-            countryDict.Add("Luxembourg", 0.14);
-            countryDict.Add("Malta", 0.11);
-            countryDict.Add("Netherlands", 3.89);
-            countryDict.Add("Poland", 8.49);
-            countryDict.Add("Portugal", 2.30);
-            countryDict.Add("Romania", 4.34);
-            countryDict.Add("Slovakia", 1.22);
-            countryDict.Add("Slovenia", 0.47);
-            countryDict.Add("Spain", 10.49);
-            countryDict.Add("Sweden", 2.29);
 
             int yes = 27;
             int no = 0;
@@ -48,7 +18,9 @@ namespace votingCalculator
             double percNo = 0.00;
             double percAb = 0.00;
 
-            foreach (var country in countryDict)
+            Countries c = new Countries();
+
+            foreach (var country in c.countryDict)
             {
                 Console.WriteLine(country.Key);
                 string vote = Console.ReadLine();
