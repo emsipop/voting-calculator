ccc98f85faa7725a800ccef7865a6c29ac8257ad
Emma Baskerville <ecbaskerville@yahoo.com>
2020-02-21 13:09:21 +0000

    Create class to store country participation rules

--- Program.cs
+++ Program.cs
@@ -5,55 +5,17 @@ namespace votingCalculator
 {
     class Program
     {
-        // Method establishes which countries are participating
-        static void Participating()
+        static void Voting(Dictionary<string, double> countryDict)
         {
-            // If the all countries are participating, the Votes method will be called
-            // If not all countries are participating, the Display method is called and countries are removed
-            Console.WriteLine("Are all countries participating? \n 1. Yes \n 2. No");
-            int select = int.Parse(Console.ReadLine());
-            if(select == 2)
-            {
-                Display();
-                int end = 1;
-                while (end != 2)
-                {
-                    Console.WriteLine("Enter the name of the country not participating");
-                    string name = Console.ReadLine();
-                    Remove(name);
-                    Console.WriteLine("Would you like to remove another country? \n 1. Yes \n 2. No");
-                    end = int.Parse(Console.ReadLine());
-                }
-            }
-        }
-
-        // Method removes the country from the dictionary specified in the parameter
-        static void Remove(string choice)
-        {
-
-            Country remove = new Country();
-            remove.countryDict.Remove(choice);
-            Console.WriteLine($"{choice} is no longer participating \n");
-            foreach (var country in remove.countryDict)
+            Console.WriteLine("These are the countries participating: \n");
+            foreach (var country in countryDict)
             {
                 Console.WriteLine(country.Key);
             }
-        }
 
-        // Method displays a list of all the countries in the dictionary
-        static void Display()
-        {
-            Country vote = new Country();
-            foreach (var country in vote.countryDict)
-            {
-                Console.WriteLine(country.Key);
-            }
-        }
+            int listLength = countryDict.Count;
 
-        // Method runs to change each country's vote
-        static void Votes()
-        {
-            int yes = 27;
+            int yes = listLength;
             int no = 0;
             int abstain = 0;
 
@@ -61,19 +23,18 @@ namespace votingCalculator
             double percNo = 0.00;
             double percAb = 0.00;
 
-            Country remove = new Country();
-
-            foreach (var country in remove.countryDict)
+            Console.WriteLine("\nSet the votes for each country (y/n/a): \n");
+            foreach (var country in countryDict)
             {
-                Console.WriteLine(country.Key);
+                Console.Write($"{country.Key} ---> ");
                 string vote = Console.ReadLine();
                 if (vote == "y")
                 {
-                    Console.WriteLine("The Country has voted yes \n");
+                    Console.WriteLine($"{country.Key} voted yes \n");
                 }
                 else if (vote == "n")
                 {
-                    Console.WriteLine("The Country has voted no \n");
+                    Console.WriteLine($"{country.Key} voted no \n");
                     yes--;
                     no++;
                     percYes = Math.Round(percYes - country.Value, 2);
@@ -81,7 +42,7 @@ namespace votingCalculator
                 }
                 else if (vote == "a")
                 {
-                    Console.WriteLine("The Country has voted to abstain from the vote \n");
+                    Console.WriteLine($"{country.Key} have abstained \n");
                     yes--;
                     abstain++;
                     percYes = Math.Round(percYes - country.Value, 2);
@@ -89,58 +50,101 @@ namespace votingCalculator
                 }
                 else
                 {
-                    Console.WriteLine("This vote is invalid we're changing the vote to yes \n");
+                    Console.WriteLine("Not a valid input, vote has been set to yes");
                 }
             }
 
             if (percYes < 0.00)
             {
-                percYes = 0.00;
+                percYes = 0;
             }
-
             if (percNo > 100.00)
             {
                 percNo = 100.00;
             }
-
             if (percAb > 100.00)
             {
                 percAb = 100.00;
             }
 
-            Console.WriteLine("MEMBER STATES");
+            Console.WriteLine("Choose the voting rule: \n1. Qualified majority \n2. Reinforced qualified majority \n3. Simple majority \n4. Unanimity");
+            int rule = int.Parse(Console.ReadLine());
+
+            if (rule == 1)
+            {
+                if (yes < listLength * 0.55 | percYes < 65.00)
+                {
+                    Console.WriteLine("FINAL RESULT ---> REJECTED \n");
+                }
+                else
+                {
+                    Console.WriteLine("FINAL RESULT ---> APPROVED \n");
+                }
+            }
+            else if (rule == 2)
+            {
+                if (yes < listLength * 0.72 | percYes < 65.00)
+                {
+                    Console.WriteLine("FINAL RESULT ---> REJECTED \n");
+                }
+                else
+                {
+                    Console.WriteLine("FINAL RESULT ---> APPROVED \n");
+                }
+            }
+            else if (rule == 3)
+            {
+                if (yes < listLength * 0.50)
+                {
+                    Console.WriteLine("FINAL RESULT ---> REJECTED \n");
+                }
+                else
+                {
+                    Console.WriteLine("FINAL RESULT ---> APPROVED \n");
+                }
+            }
+            else if (rule == 4)
+            {
+                if (yes < listLength * 1.00)
+                {
+                    Console.WriteLine("FINAL RESULT ---> REJECTED \n");
+                }
+                else
+                {
+                    Console.WriteLine("FINAL RESULT ---> APPROVED \n");
+                }
+            }
+
+            Console.WriteLine($"{listLength} MEMBER STATES");
             Console.WriteLine($"Yes: {yes}");
             Console.WriteLine($"No: {no}");
             Console.WriteLine($"Abstain: {abstain} \n");
 
-            Console.WriteLine("POPULATION");
-            Console.WriteLine($"Yes: {percYes}%");
-            Console.WriteLine($"No: {percNo}%");
-            Console.WriteLine($"Abstain: {percAb}% \n");
-
-            if (yes < 15 | percYes < 65.00)
-            {
-                Console.WriteLine("Final result: REJECTED");
-            }
-            else
-            {
-                Console.WriteLine("Final result: APPROVED");
-            }
+            Console.WriteLine("% POPULATION");
+            Console.WriteLine($"Yes: {Math.Round(percYes, 2)}%");
+            Console.WriteLine($"No: {Math.Round(percNo, 2)}%");
+            Console.WriteLine($"Abstain: {Math.Round(percAb, 2)}%");
         }
 
-        // Main method
         static void Main(string[] args)
         {
-            // Title displayed
-            Console.WriteLine("VOTING CALCULATOR");
+            Console.WriteLine("VOTING CALCULATOR \n");
 
-            // Method called to ask which countries are participating
-            Participating();
+            CountryDicts dict = new CountryDicts();
 
-            // Method called to run through voting choices
-            // Results are then displayed
-            Console.WriteLine("Set the votes for each country:");
-            Votes();
+            Console.WriteLine("Participation Rules: \n1. All countries participating \n2. Only Eurozone countries participating");
+            int partRule = int.Parse(Console.ReadLine());
+
+            if (partRule == 1)
+            {
+                Console.WriteLine("Participation Rule: All countries participating \n");
+                Voting(dict.allCountries);
+            }
+            if (partRule == 2)
+            {
+                Console.WriteLine("Participation Rule: Only Eurozone countries participating \n");
+                Voting(dict.eurozone);
+            }
         }
     }
 }
